968f292c1a306211b85b62cbacde747c632c7094
Marian <contact@bashcole.com>
2023-02-16 14:27:56 +0200

    Fix cards on mobile

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -8,7 +8,7 @@ defineProps<{ coin: ICoin }>()
 </script>
 
 <template>
-  <div class="flex items-center justify-center order-1 xl:justify-start">
+  <div class="flex md:items-center md:justify-center order-1 xl:justify-start">
     <img :src="coin.icon" :alt="coin.name" class="self-start">
     <div class="flex flex-col ml-4">
       <span class="block text-lg font-bold text-alpha-700">{{ coin.name }}</span>
